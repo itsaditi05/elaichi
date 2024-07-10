2a05628c58e3505f54f4dd30a7bdeed1b8d3393e
itsaditi05 <aditiupadhyay4363@gmail.com>
2024-07-10 11:01:40 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+console.log("1");
+console.log("2");
+console.log("3");
+console.log("4");
+for(i=1;i<=5;i++){
+console.log(i)};
+
+for(let i=5;i>=1;i--){
+    console.log(i);
+}
+let months=["january","july","march","august"];{
+console.log(months);
+}
+ const favmovie="avatar";
+ let guess="prompt"("guess my favorite movie");
+ 
+
